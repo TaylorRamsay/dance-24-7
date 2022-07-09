7e808ae57098a48efb0c7b87ecac199891909065
tRamsay93 <54593330+TaylorRamsay@users.noreply.github.com>
2022-07-09 13:19:00 -0700

    Added terrain, player obect/movement, basic follow cam, music
    -Flat terrain for grey-box testing
    -PlayerObject with scripts for ThirdPersonMovement, RestartScene, and basic follow cam
    -Audio object which has basic play on start script

---
+++ Dance 24-7/Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+
+    float MoveX = 0;
+    float MoveY = 0;
+    float MoveZ = 0;
+
+    float Speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        MoveX = Input.GetAxis("Horizontal") * Time.deltaTime * Speed;
+        MoveZ = Input.GetAxis("Vertical") * Time.deltaTime * Speed;
+        MoveY = Input.GetAxis("Jump") * Time.deltaTime * Speed;
+        transform.Translate(MoveX, MoveY, MoveZ);
+    }
+}
