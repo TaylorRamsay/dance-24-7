b32183a3b4c5cc893d2d490c221d6361411b787d
tRamsay93 <54593330+TaylorRamsay@users.noreply.github.com>
2022-07-12 12:51:08 -0700

    Implemented gravity, jumping, collision on player object
    - Added to ThirdPersonMovement script to include player gravity + jumping
    - Added basic PlayerCollision script included in Unity manual for player collision detection

--- Dance 24-7/Assets/Scripts/ThirdPersonMovement.cs
+++ Dance 24-7/Assets/Scripts/ThirdPersonMovement.cs
@@ -8,19 +8,55 @@ public class ThirdPersonMovement : MonoBehaviour
     public CharacterController controller;
     public Transform cam;
 
+    // Player Movement Variables
     public float speed;
     public float walkSpeed = 6f;
     public float runSpeed = 10f;
-
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
+
+    // Gravity + Jump Variables
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
     void Update()
     {
+        
+        // ====== GRAVITY + JUMP HANDLING ====== //
+        // Creates a sphere at "groundCheck.position" with radius "groundDistance" to check for collision with objects specified as "groundMask"
+        // Returns True if collision exists, False if collision does not exist
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        // Applys a constant low gravity velocity of -2f if grounded
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // Increments gravity value to player while not grounded, Time.deltaTime to keep frame rate independent
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+
+        // ====== MOVEMENT HANDLING ====== //
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
+        
+        
+        // Is there a more efficient way to do this??
         if(Input.GetKey(KeyCode.LeftShift))
         {
             speed = runSpeed;
