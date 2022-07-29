6750de001641fe77020e31fd55da9119efa2d08c
Taylor Ramsay <54593330+TaylorRamsay@users.noreply.github.com>
2022-07-29 11:20:55 -0700

    Implementing bloom effect with audio influence

---
+++ Dance 24-7/Assets/Scripts/Audio Scripts/VisualInfluencer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualInfluencer : MonoBehaviour
+{
+    public string csvPath;
+    public GameObject currentNPC;
+    //public float intensity = currentNPC.
+
+    public Color colorDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //colorDisplay = currentNPC.gameObject.GetComponent<Shader>().;
+    }
+}
