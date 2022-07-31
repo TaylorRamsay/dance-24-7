5c37ac9c810d9241a5260792c52111848a7ef8c5
tRamsay93 <54593330+TaylorRamsay@users.noreply.github.com>
2022-07-31 12:28:19 -0700

    Implemented functioning visual influencer script
    - Script is applied to "chordsNPC"
    - Applies glow effect to NPC and corresponding instrument

--- Dance 24-7/Assets/Scripts/Audio Scripts/VisualInfluencer.cs
+++ Dance 24-7/Assets/Scripts/Audio Scripts/VisualInfluencer.cs
@@ -1,24 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System;
+
+// index = (int)((currentPlaybackTime / TotalTime) * csvArraySize);
+// the first half gets some playback ratio for you to then convert to an index based on the size of your processed CSV data
+// you really only need to do this during a monobehaviour's script (once per frame
+// Iff you want to detect local maxima, you'll need to keep some values around to use as a part of the evaluation.
+// the int cast being on the full evaluation is key; if you cast the ratio calculation to int, the expression will always evaluate to 0.
+// the more sample points you have in your CSV, the more your effects will line up.
+// As an additional suggestion for your own testing purposes, I'd recommend you come up with a few, differently grained data sets for the same piece of music.
+// Give yourself the ability to swap those data sets in so you can evaluate the look/feel on the fly.
+// It will help you make an informed decision on what data sizes "feel" best
 
 public class VisualInfluencer : MonoBehaviour
 {
-    public string csvPath;
+    public FMODUnity.StudioEventEmitter track;
+    public string csvPath = @"C:\Users\FSK8475\Documents\GitHub\dance-24-7\Dance 24-7\Assets\CVS Music Data\Chira Stem Data\chords.csv";
     public GameObject currentNPC;
-    //public float intensity = currentNPC.
+    public Material currentMaterial;
+    public float intesityFactor;
+    private List<float> musicData = new List<float>();
+    public int currTime = 0;
+    public int totalTime = 0;
+    private int index = 0;
 
-    public Color colorDisplay;
-
-    // Start is called before the first frame update
-    void Start()
+    void ReadCSV(ref List<float> csvList)
     {
-        
+        if (File.Exists(csvPath))
+        { 
+            var lines = File.ReadLines(csvPath);
+            foreach (var line in lines)
+            { 
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    continue;
+                }
+                else
+                {
+                    float c = float.Parse(line);
+                    csvList.Add(c);
+                }
+            }   
+        }
+
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        ReadCSV(ref musicData);
+        track.EventDescription.getLength(out totalTime);
+        currentMaterial.GetColor("_EmissionColor");
+    }
+
     void Update()
     {
-        //colorDisplay = currentNPC.gameObject.GetComponent<Shader>().;
+        print("------------Current Visual Influence Info (Chords)---------------");
+        print("Current time:" + currTime + "\n\tTotal time: " + totalTime + "\nList Size: " + musicData.Count);
+
+        track.EventInstance.getTimelinePosition(out currTime);
+        index = (int)(((float)currTime / (float)totalTime) * musicData.Count);
+
+        print("Current index: " + index);
+        print("Current List Item: " + musicData[index]);
+
+        Color glow = currentMaterial.GetColor("_Color");
+        glow *= Mathf.Pow(2.0F, musicData[index]);
+        currentMaterial.SetColor("_EmissionColor", glow);
     }
 }
